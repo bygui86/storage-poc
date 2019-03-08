dc086329c16984e3d6034a03e45827fad4e8d233
Matteo Baiguini <baiguini.matteo@gmail.com>
2019-03-08 14:52:41 +0100

    Added mongodb prototype

---
+++ mongodb-service/README.md
@@ -0,0 +1,17 @@
+
+# SwissBlock - Storage PoCs - MongoDB replica set
+
+## Run
+
+### Locally
+```
+cd <PROJECT_ROOT_FOLDER>/mongodb-replica-set
+docker run -d --name mongo --restart always -p 27017:27017 mongo
+make clean run
+```
+
+---
+
+## Links
+
+-
---
+++ mongodb-service/src/main/java/com/swissblock/storagepoc/mongodb/configs/PersistenceConfig.java
@@ -0,0 +1,12 @@
+package com.swissblock.storagepoc.mongodb.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+
+@Configuration
+// @EnableMongoAuditing
+public class PersistenceConfig {
+
+	// no-op
+}
